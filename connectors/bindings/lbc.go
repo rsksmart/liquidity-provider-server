ef8343eddfbd8040a1be0402af3022adbb45d0f0
aguschirico <aguschirico@gmail.com>
2021-09-17 12:57:06 +0200

    add comment.

--- connectors/bindings/lbc.go
+++ connectors/bindings/lbc.go
@@ -547,7 +547,7 @@ func (l *LBCTransactor) RegisterPegInWithoutTx(q LiquidityBridgeContractQuote, s
 	err := l.contract.Call(opts, &res, "registerPegIn", q, signature, tx, pmt, height)
 	if err != nil {
 		if strings.Contains(err.Error(), "Failed to validate BTC transaction") {
-			return nil
+			return nil // allow retrying in case the bridge didn't acknowledge all required confirmations have occured
 		}
 		return err
 	}
