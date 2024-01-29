bbab2d7ea4509b667d1faebde8b8554f4124487c
Luisfc68 <luisfc68@gmail.com>
2024-01-29 11:49:58 -0300

    ci: update configuration files and dependencies

--- docker-compose/lps/start.sh
+++ docker-compose/lps/start.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-echo "Detected LPS_STAGE: $LPS_STAGE, LBC_ADDR: $LBC_ADDR, BTCD_RPC_USER: $BTCD_RPC_USER, RSK_CHAIN_ID: $RSK_CHAIN_ID"
+echo "Detected LPS_STAGE: $LPS_STAGE, LBC_ADDR: $LBC_ADDR, BTC_USERNAME: $BTC_USERNAME, RSK_CHAIN_ID: $RSK_CHAIN_ID"
 
 echo "Testing if we have a default wallet"
 
-curl -s "http://bitcoind01:5555" --user "$BTCD_RPC_USER:$BTCD_RPC_PASS" -H "Content-Type: application/json" -d '{"jsonrpc": "1.0", "method": "getwalletinfo", "params": [], "id":"getwallet"}' | grep "{\"result\":null,\"error\":{\"code\":-18" \
+curl -s "http://bitcoind01:5555" --user "$BTC_USERNAME:$BTC_PASSWORD" -H "Content-Type: application/json" -d '{"jsonrpc": "1.0", "method": "getwalletinfo", "params": [], "id":"getwallet"}' | grep "{\"result\":null,\"error\":{\"code\":-18" \
   && echo "No default wallet" \
   && echo "Creating wallet" \
-  && curl -s "http://bitcoind01:5555" --user "$BTCD_RPC_USER:$BTCD_RPC_PASS" -H "Content-Type: application/json" -d '{"jsonrpc": "1.0", "method": "createwallet", "params": ["main", false, false, "test-password", true, false, true], "id":"createwallet"}'
+  && curl -s "http://bitcoind01:5555" --user "$BTC_USERNAME:$BTC_PASSWORD" -H "Content-Type: application/json" -d '{"jsonrpc": "1.0", "method": "createwallet", "params": ["main", false, false, "test-password", true, false, true], "id":"createwallet"}'
 
 echo "Starting LP Server..."
 liquidity-provider-server
