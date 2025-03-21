b979b5d1d780d17c3a68a395257f9a7fbf470279
Luisfc68 <luisfc68@gmail.com>
2025-03-21 20:22:42 -0300

    fix: sed usage to run in pipeline

--- docker-compose/local/lps-env.sh
+++ docker-compose/local/lps-env.sh
@@ -33,7 +33,7 @@ echo "LPS_STAGE: $LPS_STAGE; ENV_FILE: $ENV_FILE; LPS_UID: $LPS_UID"
 
 # Force Management API to be enabled
 if [ -f "$ENV_FILE" ]; then
-  sed -i '' 's/^ENABLE_MANAGEMENT_API=.*/ENABLE_MANAGEMENT_API=true/' "$ENV_FILE"
+  sed -i 's/^ENABLE_MANAGEMENT_API=.*/ENABLE_MANAGEMENT_API=true/' "$ENV_FILE"
 fi
 
 SCRIPT_CMD=$1
