59d145dab05fbcc77a2955b192f6a7760b16d04e
AndresQuijano <andres.quijano@iovlabs.org>
2025-12-03 19:07:43 -0500

    test: add missing tests for provider manager handlers

--- internal/adapters/entrypoints/rest/handlers/resignation.go
+++ internal/adapters/entrypoints/rest/handlers/resignation.go
@@ -1,17 +1,21 @@
 package handlers
 
 import (
-	"github.com/rsksmart/liquidity-provider-server/internal/adapters/entrypoints/rest"
-	"github.com/rsksmart/liquidity-provider-server/internal/usecases/liquidity_provider"
 	"net/http"
+
+	"github.com/rsksmart/liquidity-provider-server/internal/adapters/entrypoints/rest"
 )
+
+type ResignUseCase interface {
+	Run() error
+}
 
 // NewResignationHandler
 // @Title Provider resignation
 // @Description Provider stops being a liquidity provider
 // @Route /providers/resignation [post]
 // @Success 204 object
-func NewResignationHandler(useCase *liquidity_provider.ResignUseCase) http.HandlerFunc {
+func NewResignationHandler(useCase ResignUseCase) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		err := useCase.Run()
 		if err != nil {
