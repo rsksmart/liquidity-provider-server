4c2eb85efac5c4ca0b595de70a1d634210f69ffa
Volodymyr Zahanych <volodymyr.zahanych@ext.rootstocklabs.com>
2026-06-08 13:37:19 +0200

    fix: add structured logs to pegin/pegout status use cases
    Log repository load failures at Error and not-found/not-accepted at Debug,
    with quoteHash + error fields via logrus WithField/WithError (FLY-2344).

--- internal/usecases/pegin/pegin_status.go
+++ internal/usecases/pegin/pegin_status.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/rsksmart/liquidity-provider-server/internal/entities/quote"
 	"github.com/rsksmart/liquidity-provider-server/internal/usecases"
+	log "github.com/sirupsen/logrus"
 )
 
 type StatusUseCase struct {
@@ -17,14 +18,18 @@ func NewStatusUseCase(quoteRepository quote.PeginQuoteRepository) *StatusUseCase
 func (useCase *StatusUseCase) Run(ctx context.Context, quoteHash string) (quote.WatchedPeginQuote, error) {
 	peginQuote, err := useCase.quoteRepository.GetQuote(ctx, quoteHash)
 	if err != nil {
+		log.WithField("quoteHash", quoteHash).WithError(err).Error("QuoteStatus: repository load failed")
 		return quote.WatchedPeginQuote{}, usecases.WrapUseCaseError(usecases.PeginQuoteStatusId, err)
 	} else if peginQuote == nil {
+		log.WithField("quoteHash", quoteHash).Debug("QuoteStatus: quote not found")
 		return quote.WatchedPeginQuote{}, usecases.WrapUseCaseError(usecases.PeginQuoteStatusId, usecases.QuoteNotFoundError)
 	}
 	retainedQuote, err := useCase.quoteRepository.GetRetainedQuote(ctx, quoteHash)
 	if err != nil {
+		log.WithField("quoteHash", quoteHash).WithError(err).Error("QuoteStatus: repository load failed")
 		return quote.WatchedPeginQuote{}, usecases.WrapUseCaseError(usecases.PeginQuoteStatusId, err)
 	} else if retainedQuote == nil {
+		log.WithField("quoteHash", quoteHash).Debug("QuoteStatus: quote not yet accepted")
 		return quote.WatchedPeginQuote{}, usecases.WrapUseCaseError(usecases.PeginQuoteStatusId, usecases.QuoteNotAcceptedError)
 	}
 	creationData := useCase.quoteRepository.GetPeginCreationData(ctx, quoteHash)
--- internal/usecases/pegout/pegout_status.go
+++ internal/usecases/pegout/pegout_status.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/rsksmart/liquidity-provider-server/internal/entities/quote"
 	"github.com/rsksmart/liquidity-provider-server/internal/usecases"
+	log "github.com/sirupsen/logrus"
 )
 
 type StatusUseCase struct {
@@ -17,14 +18,18 @@ func NewStatusUseCase(quoteRepository quote.PegoutQuoteRepository) *StatusUseCas
 func (useCase *StatusUseCase) Run(ctx context.Context, quoteHash string) (quote.WatchedPegoutQuote, error) {
 	pegoutQuote, err := useCase.quoteRepository.GetQuote(ctx, quoteHash)
 	if err != nil {
+		log.WithField("quoteHash", quoteHash).WithError(err).Error("QuoteStatus: repository load failed")
 		return quote.WatchedPegoutQuote{}, usecases.WrapUseCaseError(usecases.PegoutQuoteStatusId, err)
 	} else if pegoutQuote == nil {
+		log.WithField("quoteHash", quoteHash).Debug("QuoteStatus: quote not found")
 		return quote.WatchedPegoutQuote{}, usecases.WrapUseCaseError(usecases.PegoutQuoteStatusId, usecases.QuoteNotFoundError)
 	}
 	retainedQuote, err := useCase.quoteRepository.GetRetainedQuote(ctx, quoteHash)
 	if err != nil {
+		log.WithField("quoteHash", quoteHash).WithError(err).Error("QuoteStatus: repository load failed")
 		return quote.WatchedPegoutQuote{}, usecases.WrapUseCaseError(usecases.PegoutQuoteStatusId, err)
 	} else if retainedQuote == nil {
+		log.WithField("quoteHash", quoteHash).Debug("QuoteStatus: quote not yet accepted")
 		return quote.WatchedPegoutQuote{}, usecases.WrapUseCaseError(usecases.PegoutQuoteStatusId, usecases.QuoteNotAcceptedError)
 	}
 	creationData := useCase.quoteRepository.GetPegoutCreationData(ctx, quoteHash)
